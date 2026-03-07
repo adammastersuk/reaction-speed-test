f69b9097ea5f301da496c82eefd4f3fa1a9ca710
Adam Masters <106275725+adammastersuk@users.noreply.github.com>
2026-03-07 10:02:41 +0000

    Add basePath /reaction-speed-test for builds hub routing

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  basePath: '/reaction-speed-test'
 };
 
 module.exports = nextConfig;
